36428fff80f5cfc61fd07d8349fddbd53c0885a0
James Little <littleguy23@gmail.com>
2019-06-23 21:11:07 -0400

    Initial Commit

---
+++ src/components/Login.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="login">
+        <form action="#" v-on:submit.prevent="submit">
+            <p><label for="email">Email: <input type="text" name="email" v-model="email"></label></p>
+            <p><label for="password">Password: <input type="password" name="password" id="" v-model="password"></label></p>
+            <p><button type="submit">Log in</button></p>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const a = axios.create({
+    baseURL: 'http://back.test/api/'
+})
+
+export default {
+    data() {
+        return {
+            email: "1@gmail.com",
+            password: "qwerty",
+        }
+    },
+
+    methods: {
+        submit: function() {
+            var self = this;
+            a.post('/login', {
+                email: this.email,
+                password: this.password
+            }).then(function(response) {
+                console.log(self.$store);
+                self.$store.commit('setApiKey', response.data.api_token);
+            }).catch(function(error) {
+                console.error(error);
+            })
+        }
+    }
+}
+</script>
