de202a7795e9d44a01793477ab0cf9b455e00d35
James Little <littleguy23@gmail.com>
2019-06-24 22:08:58 -0400

    Add base components to frontend

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <div id="app">
-    <Login v-if="this.loggedIn"/>
-    {{ this.$store.state.apiToken }}
+    <Login v-if="!this.loggedIn"/>
+    <div v-if="this.loggedIn">
+      <UserInfo v-bind:user="this.user"/>
+      <SplitCreate />
+      <TransactionList />
+      <DebtsList />
+    </div>
   </div>
 </template>
 
 <script>
 import Login from './components/Login.vue'
+import UserInfo from './components/UserInfo.vue'
+import SplitCreate from './components/SplitCreate.vue'
+import TransactionList from './components/TransactionList.vue'
+import DebtsList from './components/DebtsList.vue'
 
 export default {
   name: 'app',
   computed: {
     loggedIn () {
-      return this.$store.state.apiToken == null;
+      return this.$store.state.apiToken !== null;
     }
   },
   components: {
-    Login
+    Login, UserInfo, SplitCreate, TransactionList, DebtsList
   }
 }
 </script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        TXN List
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const a = axios.create({
+    baseURL: 'http://back.test/api/'
+})
+
+export default {
+    data() {
+        return {
+            email: "1@gmail.com",
+            password: "qwerty",
+        }
+    },
+
+    methods: {
+        submit: function() {
+            var self = this;
+            a.post('/login', {
+                email: this.email,
+                password: this.password
+            }).then(function(response) {
+                self.$store.commit('setApiKey', response.data.api_token);
+            }).catch(function(error) {
+                console.error(error);
+            })
+        }
+    }
+}
+</script>
