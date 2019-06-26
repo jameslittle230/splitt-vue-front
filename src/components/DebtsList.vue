fadf46909f4abbd8796abfb6af043b5193e9d383
James Little <littleguy23@gmail.com>
2019-06-26 13:46:40 -0400

    Clean up debts component

--- src/components/DebtsList.vue
+++ src/components/DebtsList.vue
@@ -11,27 +11,13 @@
 </template>
 
 <script>
-import axios from 'axios';
-
-const a = axios.create({
-    baseURL: 'http://back.test/api/'
-})
 
 export default {
-    data() {
-        return {
-            email: "1@gmail.com",
-            password: "qwerty",
-        }
-    },
-
     computed: {
         debts: function() {
             return this.$store.state.me.splits
         }
     },
 
-    methods: {
-    }
 }
 </script>
