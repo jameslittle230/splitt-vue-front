dfdb25cc05a623956a3bc6eddacc40283a57ebd0
James Little <littleguy23@gmail.com>
2019-06-26 23:03:57 -0400

    Better display of debts list

--- src/components/DebtsList.vue
+++ src/components/DebtsList.vue
@@ -1,13 +1,16 @@
 <template>
     <div v-if="this.$store.state.currentGroup">
-        <h2>Debts List</h2>
+        <h2>Debts List (You owe <MoneyDisplay v-bind:amount="debtTotal" />)</h2>
         <ul>
             <li v-for="debt in debts" v-bind:key="debt.id">
-                <strong><MoneyDisplay v-bind:amount="debt.amount" />:</strong>
-                <!-- {{debt.transaction}} -->
-                {{txns[debt.transaction].description}} ({{debt.percentage}}%)
+                You owe {{ groupMembers[txns[debt.transaction].creator].name }}
+                <strong><MoneyDisplay v-bind:amount="debt.amount" /></strong> for
+                {{txns[debt.transaction].description}} 
+                <span v-if="debt.percentage != 100">({{debt.percentage}}%)</span>
+                <button v-on:click="reconcile(debt.id)">Paid (not wired yet)</button>
             </li>
         </ul>
+        <p v-if="debts.length == 0">No debts yet!</p>
     </div>
 </template>
 
@@ -17,7 +20,20 @@ import MoneyDisplay from './MoneyDisplay';
 export default {
     computed: {
         debts: function() {
-            return this.$store.state.me.splits
+            const myId = this.$store.state.me.id;
+            return this.$store.state.currentGroup.transactions
+                .filter(txn => txn.creator != myId)
+                .map(txn => txn.splits
+                    .filter(split => split.debtor == myId)[0]
+                )
+        },
+
+        groupMembers: function() {
+            var output = [];
+            for (const member of this.$store.state.currentGroup.members) {
+                output[member.id] = member;
+            }
+            return output;
         },
 
         txns: function() {
@@ -26,6 +42,15 @@ export default {
                 output[txn.id] = txn;
             }
             return output;
+        },
+
+        debtTotal: function() {
+            const sum = (accumulator, currentValue) => accumulator + currentValue;
+            if (this.debts.length < 1) {
+                return 0
+            }
+
+            return this.debts.map(split => parseFloat(split.amount)).reduce(sum);
         }
     },
 
