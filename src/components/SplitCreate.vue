be2a27f98c8e994725664b1cef5bea8ccb05ec46
James Little <littleguy23@gmail.com>
2019-07-02 17:03:25 -0400

    Add user interface to define split percentages

--- src/components/SplitCreate.vue
+++ src/components/SplitCreate.vue
@@ -1,7 +1,10 @@
 <template>
   <div v-if="this.$store.state.currentGroup">
     <h2>New Split</h2>
-    <form action="#" method="post" v-on:submit.prevent="submit">
+    <form action="#" method="post" v-on:submit.prevent="submit"
+      v-bind:style="boxStyle"
+    >
+      <p v-if="active"><button v-on:click="active = false">Never Mind</button></p>
       $
       <VueNumeric
         separator=","
@@ -9,16 +12,41 @@
         v-bind:min="0"
         v-bind:max="100000"
         v-model="amount"
-        v-on:blur="inputIsFocused = false"
-        v-on:focus="inputIsFocused = true"
+        v-on:focus="active = true"
       />
-      <p v-if="active">
-        <label for="memo">
-          Memo:
-          <input type="text" name="memo" v-model="memo">
-        </label>&nbsp;
-        <button type="submit">Create Split</button>
-      </p>
+
+      <div v-if="active">
+        <p>
+          <label for="memo">
+            Memo:
+            <input type="text" name="memo" v-model="memo">
+          </label>
+        </p>
+
+        <input type="radio" id="reimbursement" value="reimbursement" v-model="splitViewModel.type">
+        <label for="reimbursement">reimbursement</label>
+        <input type="radio" id="split" value="split" v-model="splitViewModel.type">
+        <label for="split">split</label>
+
+        <p v-for="(member, idx) in groupMembers" v-bind:key="idx">
+          <label>{{member.name}}
+
+            <VueNumeric 
+              separator=","
+              v-bind:precision="0"
+              v-bind:min="0"
+              v-bind:max="100"
+
+              v-model="splitViewModel['memberData'][member.email]['percentage']"
+            />
+
+            % — ${{ (splitViewModel['memberData'][member.email]['amount'] / 100).toFixed(2) }}
+          </label>
+        </p>
+
+        <p><button type="submit" v-bind:disabled="percentageSum != 100">
+          {{percentageSum == 100 ? "Create Split" : "Percentages must sum to 100%"}}</button></p>
+      </div>
     </form>
   </div>
 </template>
@@ -34,15 +62,48 @@ const a = axios.create({
 export default {
   data() {
     return {
-      amount: "",
+      amount: 0,
       memo: "",
-      inputIsFocused: false
+      active: false,
+      splitViewModel: {},
     };
   },
 
   computed: {
-    active: function() {
-      return this.amount != "" || this.inputIsFocused;
+    boxStyle: function() {
+      return this.active ? {border: `1px dashed black`, padding: `20px`} : {}
+    },
+
+    groupMembers: function() {
+      return this.$store.state.currentGroup.members
+    },
+
+    percentageSum: function() {
+      return Object.keys(this.splitViewModel.memberData)
+        .map(k => this.splitViewModel.memberData[k])
+        .reduce((a, curr) => a + curr.percentage, 0);
+    },
+
+    percentageWatchKey: function() {
+      return this.splitViewModel.memberData ? Object.keys(this.splitViewModel.memberData)
+        .map(k => this.splitViewModel.memberData[k])
+        .reduce((a, curr) => `${a} ${curr.percentage}`, "") : null
+    },
+
+    splitNetworkObject: function() {
+      const obj = this.splitViewModel.memberData
+      const myEmail = this.$store.state.me.email;
+
+      return Object.keys(obj)
+        .map(function(k) {
+          var out = JSON.parse(JSON.stringify(obj[k]))
+          out.user = k
+          out.amount = parseFloat(out.amount)
+          delete out.percentageManuallySet
+          return out
+        })
+        .filter(o => o.amount > 0)
+        .filter(o => o.user != myEmail)
     }
   },
 
@@ -57,7 +118,8 @@ export default {
         method: "post",
         data: {
           full_amount: this.amount,
-          description: this.memo
+          description: this.memo,
+          splits: this.splitNetworkObject,
         },
         params: {
           api_token: this.$store.state.apiToken
@@ -67,12 +129,88 @@ export default {
           self.$store.commit("addTransaction", response.data);
           self.amount = "";
           self.memo = "";
-          self.inputIsFocused = false;
         })
         .catch(function(error) {
           console.error(error);
         });
-    }
+    },
+
+    resetSplitViewModel: function() {
+      var memberData = {};
+      const self = this;
+      this.groupMembers.forEach(function(member) {
+        memberData[member.email] = {
+          amount: 0,
+          percentage: 100 / self.groupMembers.length,
+          percentageManuallySet: false,
+        };
+      });
+
+      this.splitViewModel = {
+        type: "split",
+        memberData: memberData,
+      };
+    },
+
+    // resetManualSets: function() {
+    //   for (const email in this.splitViewModel.memberData) {
+    //     if (this.splitViewModel.memberData.hasOwnProperty(email)) {
+    //       const element = this.splitViewModel.memberData[email];
+    //       // element.amountManuallySet = false;
+    //       element.percentageManuallySet = false;
+    //     }
+    //   }
+    // },
+
+    recalculateValues: function() {
+      const myEmail = this.$store.state.me.email;
+      var totalSoFar = 0;
+
+      for (const email in this.splitViewModel.memberData) {
+        if (this.splitViewModel.memberData.hasOwnProperty(email)) {
+          const element = this.splitViewModel.memberData[email];
+          const amount = Math.floor(Math.round(this.amount * 100) * (element.percentage / 100));
+          element.amount = amount;
+          totalSoFar += amount;
+        }
+      }
+
+
+      var amountLeftOver = this.amount * 100 - totalSoFar
+      this.splitViewModel.memberData[myEmail].amount += amountLeftOver;
+    },
+
+    resetPercentages: function(type) {
+      const percentageOffset = type == "reimbursement" ? 1 : 0
+      const myEmail = this.$store.state.me.email;
+
+      for (const email in this.splitViewModel.memberData) {
+        if (this.splitViewModel.memberData.hasOwnProperty(email)) {
+          const element = this.splitViewModel.memberData[email];
+          element.percentage = (type == "reimbursement" && email == myEmail) ? 0 : 100 / (this.groupMembers.length - percentageOffset);
+        }
+      }
+    },
+  },
+
+  mounted: function() {
+    this.resetSplitViewModel();
+  },
+
+  watch: {
+    amount: function() {
+      this.recalculateValues();
+    },
+
+    "splitViewModel.type": function(newVal) {
+      this.resetPercentages(newVal)
+      this.recalculateValues();
+      // this.resetManualSets();
+    },
+
+    percentageWatchKey: function() {
+      this.recalculateValues();
+    },
   }
 };
 </script>
